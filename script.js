ae8200ae449218fd0240cb449c4456d56fee6b23
Hüseynağa Haqverdiyev <huseynagahaqverdiyev@Huseynagas-MacBook-Pro.local>
2022-12-09 11:44:55 +0400

    continent search option added

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 let containerEl = document.querySelector(".container");
 let inputEl = document.getElementById("searchbar");
+let errorEl = document.querySelector(".error");
+let continentsEl = document.getElementById("continents");
 
 function fillBoard() {
     fetch("https://restcountries.com/v3.1/all")
@@ -9,6 +11,7 @@ function fillBoard() {
         .then(data => {
             console.log(data);
             data.forEach(element => {
+                errorEl.innerHTML = "";
                 containerEl.innerHTML += `<div class="card">
                <div class="flag"><img src="${element.flags.svg}" alt=""></div>
                <div class="info"><h1>${element.name.common}</h1>
@@ -23,22 +26,61 @@ function fillBoard() {
 
 fillBoard();
 
-function searchCountry(country){
-    fetch(`https://restcountries.com/v3.1/name/${country}`)
-    .then(res => res.json())
-    .then(data => {
-        containerEl.innerHTML="";
-        console.log(data);
-        data.forEach(element => {
-            containerEl.innerHTML += `<div class="card">
+function selectContinent(continent) {
+    fetch(`https://restcountries.com/v3.1/region/${continent}`)
+        .then(res => res.json())
+        .then(data => {
+            errorEl.innerHTML = "";
+            containerEl.innerHTML = "";
+            console.log(data);
+            data.forEach(element => {
+                containerEl.innerHTML += `<div class="card">
            <div class="flag"><img src="${element.flags.svg}" alt=""></div>
            <div class="info"><h1>${element.name.common}</h1>
            <h3>Population: ${element.population}</h3>
            <h3>Region: ${element.region}</h3>
            <h3>Capital: ${element.capital}</h3></div>
        </div>`
-        });
-    })
+            })
+
+        })
 }
 
-inputEl.addEventListener("input", searchCountry(inputEl.value))
+function searchCountry(country) {
+    fetch(`https://restcountries.com/v3.1/name/${country}`)
+        .then(res => res.json())
+        .then(data => {
+            errorEl.innerHTML = "";
+            containerEl.innerHTML = "";
+            console.log(data);
+            data.forEach(element => {
+                containerEl.innerHTML += `<div class="card">
+           <div class="flag"><img src="${element.flags.svg}" alt=""></div>
+           <div class="info"><h1>${element.name.common}</h1>
+           <h3>Population: ${element.population}</h3>
+           <h3>Region: ${element.region}</h3>
+           <h3>Capital: ${element.capital}</h3></div>
+       </div>`
+            })
+
+        })
+        .catch(err => {
+            errorEl.innerHTML = `This country not found`
+        })
+}
+
+inputEl.addEventListener("input", function () {
+    if (inputEl.value == "") {
+        fillBoard()
+    } else {
+        searchCountry(inputEl.value)
+    }
+})
+
+continentsEl.addEventListener("change", function () {
+    if (containerEl.value == "all") {
+        fillBoard()
+    } else {
+        selectContinent(continentsEl.value);
+    }
+})
